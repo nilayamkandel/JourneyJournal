25bd9c9ec800debc950e3bf0b91a15d490a00445
Neelayam <67535418+Triana9@users.noreply.github.com>
2022-03-19 21:57:51 +0545

    landing page created

--- app/src/main/java/np/com/neelayamkandel/journeyjournal/presentation/activity/HomeActivity.java
+++ app/src/main/java/np/com/neelayamkandel/journeyjournal/presentation/activity/HomeActivity.java
@@ -3,10 +3,20 @@ package np.com.neelayamkandel.journeyjournal.presentation.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toolbar;
 
 import np.com.neelayamkandel.journeyjournal.R;
 
 public class HomeActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.option_menu, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
