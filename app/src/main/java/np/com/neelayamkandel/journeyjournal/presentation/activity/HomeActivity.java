4181acf91a3d297fc584eedb9b096cdd0b0294fc
Triana9 <neelayamdottkandel@gmail.com>
2022-03-19 20:52:12 +0545

    Initial commit: fragment created

---
+++ app/src/main/java/np/com/neelayamkandel/journeyjournal/presentation/activity/HomeActivity.java
@@ -0,0 +1,16 @@
+package np.com.neelayamkandel.journeyjournal.presentation.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import np.com.neelayamkandel.journeyjournal.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
